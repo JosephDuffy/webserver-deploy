4fa7198170b2f3a6125bff3219bea34975343a56
Joseph Duffy <commits@josephduffy.co.uk>
2021-05-24 19:00:24 +0100

    Add webhook for reloading containers

--- data/docker/webhook/update-container.sh
+++ data/docker/webhook/update-container.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "$1"
-echo "$2"
-echo "$3"
-
-# docker-compose --verbose pull "$1"
-# docker-compose up --detach "$1"
-# docker image prune --force
+docker-compose --verbose pull "$1"
+docker-compose up --build --detach "$1"
+docker image prune --force
